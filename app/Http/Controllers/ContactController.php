5eed9aff7e8144919ef9b70d3afd5809b8b14356
Internee <Internee@LRO-SH-19.vu.edu.pk>
2023-02-06 09:14:23 +0500

    added new comment in contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,5 +1,5 @@
 <?php
-
+// my new comment
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -9,34 +9,34 @@ use App\Models\Company;
 class ContactController extends Controller
 {
    public function index(){
-        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');   
+        $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
         $contacts = Contact::orderBy('id', 'desc')->where(function($query){
             if($companyId = request('company_id')){
                 $query->where('company_id', $companyId);
             }
         })->paginate(10);
-       
+
        return view('contacts.index', compact('contacts', 'companies'));
    }
 
    public function create(){
     $contact = new Contact();
     $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
-    
+
     return view('contacts.create', compact('companies', 'contact'));
    }
 
-   public function store(Request $request){ 
+   public function store(Request $request){
     $request->validate([
         'first_name' => 'required',
         'last_name' => 'required',
         'email' => 'required|email',
         'address' => 'required',
         'company_id' => 'required|exists:companies,id',
-    ]);    
-    
+    ]);
+
     Contact::create($request->all());
-    
+
     return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
    }
 
@@ -46,28 +46,28 @@ class ContactController extends Controller
    }
 
    public function edit($id){
-    
+
     $companies = Company::orderBy('name')->pluck('name', 'id')->prepend('All Companies', '');
     $contact = Contact::findOrFail($id);
     return view('contacts.edit', compact('companies', 'contact'));
-    
+
    }
 
    public function update($id, Request $request){
-    
+
     $request->validate([
         'first_name' => 'required',
         'last_name' => 'required',
         'email' => 'required|email',
         'address' => 'required',
         'company_id' => 'required|exists:companies,id',
-    ]);    
-    
+    ]);
+
     $contact = Contact::findOrFail($id);
     $contact->update($request->all());
 
     return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
-    
+
    }
 
    public function destroy($id){
